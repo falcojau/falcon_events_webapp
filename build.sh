d9fefe908cfbeff741c9e642460f4d6e8f13c482
Jaume Falcó <jaumefalco93@gmail.com>
2025-12-29 13:29:20 +0100

    Update build.sh for superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if doesn't exist
+if [ "$CREATE_SUPERUSER" ]; then
+  python manage.py createsuperuser --no-input || echo "Superuser already exists"
+fi
